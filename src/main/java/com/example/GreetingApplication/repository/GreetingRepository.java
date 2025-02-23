551976ae3fecbed80f6ea9c2e6d949665f400f82
Sanjay rajpoot <sanjayrajpoot1165@gmail.com>
2025-02-23 23:20:44 +0530

    [Sanjay] add. list all data

--- src/main/java/com/example/GreetingApplication/repository/GreetingRepository.java
+++ src/main/java/com/example/GreetingApplication/repository/GreetingRepository.java
@@ -3,28 +3,15 @@ package com.example.GreetingApplication.repository;
 import com.example.GreetingApplication.model.Greeting;
 import org.springframework.stereotype.Repository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.HashMap;
 import java.util.Map;
 import java.util.Optional;
 
 @Repository
-public class GreetingRepository {
-
-//        @Autowired
-//        private GreetingRepository greetingRepository;
-
-    private Map<Long, Greeting> greetingMap = new HashMap<>();
-    private long idCounter = 1;
-
-    public Greeting save(Greeting greeting) {
-        greeting.setId(idCounter++);
-        greetingMap.put(greeting.getId(), greeting);
-        return greeting;
-    }
-
-    public Optional<Greeting> findById(Long id) {
-        return Optional.ofNullable(greetingMap.get(id));
-    }
+public interface GreetingRepository extends JpaRepository<Greeting, Long> {
 }
+
+
 
