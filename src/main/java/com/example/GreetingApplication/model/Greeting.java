a3c23df334974022788bb999f85d7f544ae8bfff
Sanjay rajpoot <sanjayrajpoot1165@gmail.com>
2025-02-23 22:05:14 +0530

    [Sanjay] add. parameter like id and message

---
+++ src/main/java/com/example/GreetingApplication/model/Greeting.java
@@ -0,0 +1,26 @@
+package com.example.GreetingApplication.model;
+
+public class Greeting {
+
+    private Long id;
+    private String message;
+
+    // Getters and Setters
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+}
+
+
